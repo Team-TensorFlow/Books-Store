425477f57fc27bd0c79a6bc1c7058b82bd53e940
Abdullah-Al-Faheem <abdullahalfaheem9@gmail.com>
2026-08-04 12:56:33 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send(`
+        <html>
+        <body style="
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            height:100vh;
+            font-size:60px;
+            font-family:Arial;
+        ">
+            Welcome to JU CSE!
+        </body>
+        </html>
+    `);
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
