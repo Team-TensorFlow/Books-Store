6e95eb898f70056619c07dc24fceab50fdc8c3d5
Iqramul Hasan Naeem <20230657071@juniv.edu>
2026-08-04 15:46:16 +0600

    nothing 6

--- app.js
+++ app.js
@@ -4,21 +4,36 @@ const app = express();
 
 app.get("/", (req, res) => {
     res.send(`
+        <!DOCTYPE html>
         <html>
-        <body style="
-            display:flex;
-            justify-content:center;
-            align-items:center;
-            height:100vh;
-            font-size:60px;
-            font-family:Arial;
-        ">
+        <head>
+            <title>JU CSE</title>
+            <style>
+                body {
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    font-family: Arial, sans-serif;
+                    background-color: #f2f2f2;
+                }
 
-        <b>Welcome to ju cse!!!</b>
-        </br>
-       <p><b> Developer:Naeem,Mahadi,Limon,Fahim</b></p>
-        
+                h1 {
+                    font-size: 60px;
+                    color: #0066cc;
+                }
 
+                p {
+                    font-size: 25px;
+                    color: #333;
+                }
+            </style>
+        </head>
+
+        <body>
+            <h1>Welcome to JU CSE!!!</h1>
+            <p><b>Developer: Naeem, Mahadi, Limon, Fahim</b></p>
         </body>
         </html>
     `);
